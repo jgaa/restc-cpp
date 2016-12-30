134a2fe864378871db6b54e6330017b0b2c33c41
Jarle Aase <jarle@jgaa.com>
2016-12-30 12:14:33 +0200

    Simplified test containers setup

---
+++ create-and-run-containers.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+docker ps > /dev/null
+if [ $? -eq 0 ]; then
+    echo "Building and starting Docker containers for testing"
+else
+    echo
+    echo "Cannot run docker commands. "
+    echo "Please install docker or give this user access to run docker commands!"
+    exit -1
+fi
+
+BASE=`pwd`
+
+echo "JSON mock server"
+cd "$BASE/tests/mock-server"
+./runmockserver
+
+echo "nginx mock server"
+cd "$BASE/tests/mock-nginx"
+./build_and_run.sh
+
+echo "squid mock server"
+cd "$BASE/tests/mock-squid/"
+./build_and_run.sh
+
--- tests/mock-squid/build_and_run.sh
+++ tests/mock-squid/build_and_run.sh
@@ -4,4 +4,10 @@ docker stop squid
 docker rm squid
 docker build . -t test-squid
 docker run --name squid  --link nginx:fwd -d -p 3003:3128  -t test-squid
+
+# There is a problem during boot where squid is unable to
+# resolve the linked hostname
+sleep 2
+docker exec -it squid service squid3 restart
+
 docker ps
