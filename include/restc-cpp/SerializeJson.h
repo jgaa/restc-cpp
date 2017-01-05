27f430511e1fe3c1f3268d1bfce8f6ed9cdc8ba5
Jarle Aase <jarle@jgaa.com>
2017-01-05 10:59:51 +0200

    Implemented issues from code review.

--- include/restc-cpp/SerializeJson.h
+++ include/restc-cpp/SerializeJson.h
@@ -1091,197 +1091,25 @@ private:
     serialize_properties properties_;
 };
 
-// /*! Serialize as a container
-// *
-// * Serialize only one list-object at a time. Follow
-// * the InputIterator contract.
-// * ref: http://en.cppreference.com/w/cpp/concept/InputIterator
-// */
-// template <typename objectT>
-// class IteratorFromJsonSerializer
-// {
-// public:
-//     using data_t = typename std::remove_const<typename std::remove_reference<objectT>::type>::type;
-//
-//     class Iterator : public std::iterator<
-//         std::input_iterator_tag,
-//         data_t,
-//         std::ptrdiff_t,
-//         const data_t *,
-//         data_t&> {
-//
-//     public:
-//         Iterator() {}
-//
-//         /*! Copy constructor
-//         * \note This constructor mailnly is supplied to make it++ work.
-//         *      It is not optimized for performance.
-//         *      As always, prefer ++it.
-//         */
-//         Iterator(const Iterator& it)
-//         : owner_{it.owner_}
-//         {
-//             if (it.data_) {
-//                 data_ = std::make_unique<objectT>(*it.data_);
-//             }
-//         }
-//
-//         Iterator(Iterator&& it)
-//         : owner_{it.owner_}, data_{move(it.data_)} {}
-//
-//         Iterator(IteratorFromJsonSerializer *owner)
-//         : owner_{owner} {}
-//
-//         Iterator& operator++() {
-//             prepare();
-//             fetch();
-//             return *this;
-//         }
-//
-//         Iterator operator++(int) {
-//             prepare();
-//             Iterator retval = *this;
-//             fetch();
-//             return retval;
-//         }
-//
-//         Iterator& operator = (const Iterator& it) {
-//             owner_ = it.owner_;
-//             if (it.data_) {
-//                 data_ = std::make_unique<objectT>(*it.data_);
-//             }
-//             return *this;
-//         }
-//
-//         Iterator& operator = (Iterator&& it) {
-//             owner_ = it.owner_;
-//             it.data_ = move(it.data_);
-//         }
-//
-//         bool operator == (const Iterator& other) const {
-//             prepare();
-//             return data_.get() == other.data_.get();
-//         }
-//
-//         bool operator != (const Iterator& other) const {
-//             return ! operator == (other);
-//         }
-//
-//         data_t& operator*() const {
-//             prepare();
-//             return get();
-//         }
-//
-//         data_t *operator -> () const {
-//             prepare();
-//             return data_.get();
-//         }
-//
-//     private:
-//         void prepare() const {
-//             if (virgin_) {
-//                 virgin_ = false;
-//
-//                 const_cast<Iterator *>(this)->fetch();
-//             }
-//         }
-//
-//         void fetch() {
-//             if (owner_) {
-//                 data_ = owner_->fetch();
-//             } else {
-//                 throw CannotIncrementEndException(
-//                     "this is an end() iterator.");
-//             }
-//         }
-//
-//         data_t& get() const {
-//             if (!data_) {
-//                 throw NoDataException("data_ is empty");
-//             }
-//             return *data_;
-//         }
-//
-//         IteratorFromJsonSerializer *owner_ = nullptr;
-//         std::unique_ptr<objectT> data_;
-//         mutable bool virgin_ = true;
-//     };
-//
-//
-//     using iterator_t = Iterator;
-//
-//     IteratorFromJsonSerializer(
-//         Reply& reply,
-//         const JsonFieldMapping *nameMapper = nullptr)
-//     : reply_stream_{reply}, name_mapper_{nameMapper} {}
-//
-//     iterator_t begin() {
-//         return Iterator{this};
-//     }
-//
-//     iterator_t end() {
-//         return Iterator{};
-//     }
-//
-// private:
-//     std::unique_ptr<objectT> fetch() {
-//
-//         if(state_ == State::PRE) {
-//             const auto ch = reply_stream_.Take();
-//             if (ch != '[') {
-//                 throw ParseException("Expected leading json '{'");
-//             }
-//             state_ = State::ITERATING;
-//         }
-//
-//         if (state_ == State::ITERATING) {
-//             while(true) {
-//                 const auto ch = reply_stream_.Peek();
-//                 if ((ch == ' ') || (ch == '\t') || (ch == ',') || (ch == '\n') || (ch == '\r')) {
-//                     reply_stream_.Take();
-//                     continue;
-//                 } else if (ch == '{') {
-//                     auto data = std::make_unique<objectT>();
-//                     RapidJsonDeserializer<objectT> handler(
-//                         *data, nullptr, name_mapper_);
-//                     json_reader_.Parse(reply_stream_, handler);
-//                     return move(data);
-//                 } else if (ch == ']') {
-//                     reply_stream_.Take();
-//                     state_ = State::DONE;
-//                     break;
-//                 } else {
-//                     throw ParseException("Unexpected character in input stream");
-//                 }
-//             }
-//         }
-//
-//         assert(state_ == State::DONE);
-//         return {};
-//     }
-//
-//     enum class State { PRE, ITERATING, DONE };
-//     State state_ = State::PRE;
-//     RapidJsonReader reply_stream_;
-//     rapidjson::Reader json_reader_;
-//     const JsonFieldMapping *name_mapper_;
-// };
-
+/*! Serialize a reply to a C++ class instance */
 template <typename dataT>
 void SerializeFromJson(dataT& rootData,
-                    Reply& reply,
-                    const JsonFieldMapping *nameMapper = nullptr) {
-    RapidJsonDeserializer<dataT> handler(rootData, nullptr, nameMapper);
+    Reply& reply,
+    const JsonFieldMapping *nameMapper = nullptr) {
+
+    RapidJsonDeserializer<dataT> handler(
+        rootData, nullptr, nameMapper);
     RapidJsonReader reply_stream(reply);
     rapidjson::Reader json_reader;
     json_reader.Parse(reply_stream, handler);
 }
 
+/*! Serialize a reply to a C++ class instance */
 template <typename dataT>
 void SerializeFromJson(dataT& rootData,
                     std::unique_ptr<Reply>&& reply,
                     const JsonFieldMapping *nameMapper = nullptr) {
-SerializeFromJson(rootData, *reply, nameMapper);
+    SerializeFromJson(rootData, *reply, nameMapper);
 }
 
 } // namespace
--- include/restc-cpp/restc-cpp.h
+++ include/restc-cpp/restc-cpp.h
@@ -4,7 +4,6 @@
 
 #include <string>
 #include <map>
-#include <list>
 #include <deque>
 #include <memory>
 #include <future>
