5150d681c8694ae61f3a3ef141e59b46c7a1b77e
Jarle Aase <jarle@jgaa.com>
2017-02-22 08:35:46 +0200

    Removed the obsolete Body class from code

--- include/restc-cpp/restc-cpp.h
+++ include/restc-cpp/restc-cpp.h
@@ -133,58 +133,6 @@ public:
         Proxy proxy;
     };
 
-
-
-
-//     // TODO: Switch to interface and may be specialized implementations
-//     struct Body {
-//         enum class Type {
-//             NONE,
-//             STRING,
-//             FILE
-//         };
-//
-//         Body() : eof_{true} {}
-//         Body(const std::string& body) : body_str_(body), type_{Type::STRING} {}
-//         Body(std::string&& body) : body_str_(move(body)), type_{Type::STRING} {}
-//         Body(const boost::filesystem::path& path) : path_(path), type_{Type::FILE} {}
-//
-//         bool HaveSize() const noexcept {
-//             return true;
-//         }
-//
-//         bool HaveAllDataReadyInBuffers() const noexcept {
-//             return type_ == Type::STRING;
-//         }
-//
-//         bool IsEof() const noexcept {
-//             return eof_;
-//         }
-//
-//         // Return true if we added data
-//         bool GetData(write_buffers_t& buffers);
-//
-//         // Typically the value of the content-length header
-//         std::uint64_t GetFizxedSize() const;
-//
-//         /*! Set the body up for a new run.
-//          *
-//          * This is typically done if request fails and the client wants
-//          * to re-try.
-//          */
-//         void Reset();
-//
-//     private:
-//         boost::optional<std::string> body_str_;
-//         boost::optional<boost::filesystem::path> path_;
-//         const Type type_ = Type::NONE;
-//         bool eof_ = false;
-//         std::unique_ptr<std::ifstream> file_;
-//         std::unique_ptr<std::array<char, 1024 * 8>> buffer_;
-//         std::uint64_t bytes_read_ = 0;
-//         mutable boost::optional<std::uint64_t> size_;
-//     };
-
     virtual const Properties& GetProperties() const = 0;
     virtual void SetProperties(Properties::ptr_t propreties) = 0;
     virtual std::unique_ptr<Reply> Execute(Context& ctx) = 0;
