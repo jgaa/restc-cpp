a7c268694fb1b5a797f30e4504f79ac1de98f95b
Jarle Aase <jarle@jgaa.com>
2017-03-12 13:01:29 +0200

    Fixed command line syntax issue with 'docker exec  -it' under windows.

--- tests/mock-squid/build_and_run.sh
+++ tests/mock-squid/build_and_run.sh
@@ -8,4 +8,14 @@ docker run --name restc-cpp-squid --link restc-cpp-nginx:api.example.com -d -p 3
 # There is a problem during boot where squid is unable to
 # resolve the linked hostname
 sleep 2
-docker exec -it restc-cpp-squid service squid3 restart
+
+case "$OSTYPE" in
+  msys*)    
+	echo "WINDOWS"
+	winpty docker exec -it restc-cpp-squid service squid3 restart
+	;;
+  *)
+	docker exec -it restc-cpp-squid service squid3 restart
+	;;
+esac
+
