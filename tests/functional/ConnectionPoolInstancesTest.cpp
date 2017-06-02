e8988c49451e8567bd3369cf333ef8d22317b305
Jarle Aase <jarle@jgaa.com>
2017-06-02 10:03:53 +0300

    Added test-case for bug #15

---
+++ tests/functional/ConnectionPoolInstancesTest.cpp
@@ -0,0 +1,61 @@
+#include "restc-cpp/restc-cpp.h"
+#include "restc-cpp/logging.h"
+#include "restc-cpp/ConnectionPool.h"
+
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+
+#include "../src/ReplyImpl.h"
+
+#include "restc-cpp/test_helper.h"
+#include "lest/lest.hpp"
+
+/* These url's points to a local Docker container with nginx, linked to
+ * a jsonserver docker container with mock data.
+ * The scripts to build and run these containers are in the ./tests directory.
+ */
+const string http_url = "http://localhost:3001/normal/posts";
+const string http_url_many = "http://localhost:3001/normal/manyposts";
+const string http_connection_close_url = "http://localhost:3001/close/posts";
+
+using namespace std;
+using namespace restc_cpp;
+
+const lest::test specification[] = {
+
+
+STARTCASE(UseAfterDelete) {
+
+    auto rest_client = RestClient::Create();
+    rest_client->Process([&](Context& ctx) {
+        auto repl = ctx.Get(GetDockerUrl(http_url));
+        repl->GetBodyAsString();
+    });
+
+    for(auto i = 0; i < 50000; ++i) {
+
+        rest_client = RestClient::Create();
+        rest_client->Process([&](Context& ctx) {
+            auto repl = ctx.Get(GetDockerUrl(http_url));
+            repl->GetBodyAsString();
+        });
+
+        if ((i % 100) == 0) {
+            clog << '#' << (i +1) << endl;
+        }
+    }
+
+} ENDCASE
+
+}; //lest
+
+int main( int argc, char * argv[] )
+{
+    namespace logging = boost::log;
+    logging::core::get()->set_filter
+    (
+        logging::trivial::severity >= logging::trivial::info
+    );
+    return lest::run( specification, argc, argv );
+}
