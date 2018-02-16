ec2e6d9ca76fac815b5580e074cc90a52422e785
Jarle Aase <jarle@jgaa.com>
2018-02-16 07:47:35 +0200

    Simplified header only dependencies.

--- cmake_scripts/external-projects.cmake
+++ cmake_scripts/external-projects.cmake
@@ -7,37 +7,40 @@
 set(EXTERNAL_PROJECTS_PREFIX ${CMAKE_BINARY_DIR}/external-projects)
 set(EXTERNAL_PROJECTS_INSTALL_PREFIX ${EXTERNAL_PROJECTS_PREFIX}/installed)
 
-include(GNUInstallDirs)
-
-# MUST be called before any add_executable() # https://stackoverflow.com/a/40554704/8766845
-link_directories(${EXTERNAL_PROJECTS_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR})
-include_directories($<BUILD_INTERFACE:${EXTERNAL_PROJECTS_INSTALL_PREFIX}/${CMAKE_INSTALL_INCLUDEDIR}>)
-
 include(ExternalProject)
 
-ExternalProject_Add(externalRapidJson
+ExternalProject_Add(
+    externalRapidJson
     PREFIX "${EXTERNAL_PROJECTS_PREFIX}"
     GIT_REPOSITORY "https://github.com/Tencent/rapidjson.git"
     GIT_TAG "master"
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNAL_PROJECTS_INSTALL_PREFIX} 
-        -DRAPIDJSON_BUILD_DOC=OFF 
-        -DRAPIDJSON_BUILD_EXAMPLES=OFF 
-        -DRAPIDJSON_BUILD_TESTS=OFF 
-        -DRAPIDJSON_BUILD_THIRDPARTY_GTEST=OFF
-        )
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory
+        <SOURCE_DIR>/include ${CMAKE_BINARY_DIR}/installed/include
+    LOG_DOWNLOAD ON
+    LOG_INSTALL ON
+    )
 
 set(EXTERNAL_RAPIDJSON_INCLUDE_DIR ${EXTERNAL_PROJECTS_INSTALL_PREFIX}/include/rapidjson)
 
-ExternalProject_Add(externalLest
+ExternalProject_Add(
+    externalLest
     PREFIX "${EXTERNAL_PROJECTS_PREFIX}"
     GIT_REPOSITORY "https://github.com/martinmoene/lest.git"
     GIT_TAG "master"
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNAL_PROJECTS_INSTALL_PREFIX} -DLEST_BUILD_EXAMPLE=OFF
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory
+        <SOURCE_DIR>/include ${CMAKE_BINARY_DIR}/installed/include
+    LOG_DOWNLOAD ON
+    LOG_INSTALL ON
     )
-
 
 message(STATUS "EXTERNAL_RAPIDJSON_INCLUDE_DIR: ${EXTERNAL_RAPIDJSON_INCLUDE_DIR}")
 
 if (INSTALL_RAPIDJSON_HEADERS)
     install(DIRECTORY ${EXTERNAL_RAPIDJSON_INCLUDE_DIR} DESTINATION include)
 endif()
+
+include_directories(${CMAKE_BINARY_DIR}/installed/include)
