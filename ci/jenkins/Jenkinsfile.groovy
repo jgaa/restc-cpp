52cb3ad50acddee622a9df2b851b8e98fb602fc1
Jarle Aase <jarle@jgaa.com>
2018-02-16 17:51:18 +0200

    Added better support for tests with Jenkins.

--- ci/jenkins/Jenkinsfile.groovy
+++ ci/jenkins/Jenkinsfile.groovy
@@ -22,7 +22,7 @@ pipeline {
 
         stage('Build') {
            parallel {
-                stage('Docker-build inside: ubuntu-xenial') {
+                stage('Ubuntu Xenial') {
                     agent {
                         dockerfile {
                             filename 'Dockefile.ubuntu-xenial'
@@ -37,11 +37,21 @@ pipeline {
                         sh 'pwd; ls -la'
                         sh 'rm -rf build'
                         sh 'mkdir build'
-                        sh 'cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make -j2 && ctest --no-compress-output -T Test'
+                        sh 'cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make'
+
+                        echo 'Getting ready to run tests'
+                        script {
+                            try {
+                                sh 'cd build && ctest --no-compress-output -T Test'
+                            } catch (exc) {
+                                echo 'Testing failed'
+                                currentBuild.result = 'UNSTABLE'
+                            }
+                        }
                     }
                 }
 
-                stage('Docker-build inside: debian-stretch') {
+                stage('Debian Stretch') {
                     agent {
                         dockerfile {
                             filename 'Dockerfile.debian-stretch'
@@ -56,11 +66,21 @@ pipeline {
                         sh 'pwd; ls -la'
                         sh 'rm -rf build'
                         sh 'mkdir build'
-                        sh 'cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make -j2 && ctest --no-compress-output -T Test'
+                        sh 'cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make'
+
+                        echo 'Getting ready to run tests'
+                        script {
+                            try {
+                                sh 'cd build && ctest --no-compress-output -T Test'
+                            } catch (exc) {
+                                echo 'Testing failed'
+                                currentBuild.result = 'UNSTABLE'
+                            }
+                        }
                     }
                 }
 
-                stage('Docker-build inside: debian-testing') {
+                stage('Debian Testing') {
                     agent {
                         dockerfile {
                             filename 'Dockefile.debian-testing'
@@ -75,11 +95,50 @@ pipeline {
                         sh 'pwd; ls -la'
                         sh 'rm -rf build'
                         sh 'mkdir build'
-                        sh 'cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make -j2 && ctest --no-compress-output -T Test'
+                        sh 'cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make'
+
+                        echo 'Getting ready to run tests'
+                        script {
+                            try {
+                                sh 'cd build && ctest --no-compress-output -T Test'
+                            } catch (exc) {
+                                echo 'Testing failed'
+                                currentBuild.result = 'UNSTABLE'
+                            }
+                        }
                     }
                 }
 
-                stage('macOS-build: ') {
+                stage('Fedora') {
+                    agent {
+                        dockerfile {
+                            filename 'Dockerfile.fedora'
+                            dir 'ci/jenkins'
+                            label 'master'
+                        }
+                    }
+
+                    steps {
+                        echo "Building on Fedora in ${WORKSPACE}"
+                        checkout scm
+                        sh 'pwd; ls -la'
+                        sh 'rm -rf build'
+                        sh 'mkdir build'
+                        sh 'cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make'
+
+                        echo 'Getting ready to run tests'
+                        script {
+                            try {
+                                sh 'cd build && ctest --no-compress-output -T Test'
+                            } catch (exc) {
+                                echo 'Testing failed'
+                                currentBuild.result = 'UNSTABLE'
+                            }
+                        }
+                    }
+                }
+
+                stage('macOS') {
                     agent {label 'macos'}
 
                     environment {
@@ -95,86 +154,101 @@ pipeline {
                         sh 'pwd; ls -la'
                         sh 'rm -rf build'
                         sh 'mkdir build'
-                        sh 'cd build && cmake -DCMAKE_BUILD_TYPE=Release -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DOPENSSL_LIBRARIES=/usr/local/opt/openssl/lib -DBOOST_LIBRARIES=/usr/local/opt/boost/lib -DBOOSTL_ROOT_DIR=/usr/local/opt/boost -DZLIB_INCLUDE_DIRS=/usr/local/opt/zlib/include/ -DZLIB_LIBRARIES=/usr/local/opt/zlib/lib/ .. && make -j4 && ctest --no-compress-output -T Test'
-                    }
-                }
+                        sh 'cd build && cmake -DCMAKE_BUILD_TYPE=Release -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DOPENSSL_LIBRARIES=/usr/local/opt/openssl/lib -DBOOST_LIBRARIES=/usr/local/opt/boost/lib -DBOOSTL_ROOT_DIR=/usr/local/opt/boost -DZLIB_INCLUDE_DIRS=/usr/local/opt/zlib/include/ -DZLIB_LIBRARIES=/usr/local/opt/zlib/lib/ .. && make -j4'
 
-                stage('Windows-MSVCx64') {
-
-                    agent {label 'windows'}
-
-                     steps {
-                        echo "Building on Windows in ${WORKSPACE}"
-                        checkout scm
-
-                        bat script: '''
-
-                        PATH=%PATH%;C:\\devel\\zlib-1.2.11\\Release;C:\\devel\\openssl\\lib-x64\\bin;C:\\local\\boost_1_64_0\\lib64-msvc-14.1
-                        rmdir /S /Q build
-                        mkdir build
-                        cd build
-                        cmake -DCMAKE_PREFIX_PATH=c:/devel/zlib-1.2.11;c:/devel/zlib-1.2.11/Release;c:/devel/openssl;c:/devel/boost_1_64_0 -G "Visual Studio 15 Win64" ..
-                        if %errorlevel% neq 0 exit /b %errorlevel%
-                        cmake --build . --config Release
-                        if %errorlevel% neq 0 exit /b %errorlevel%
-                        ctest -C Release
-                        if %errorlevel% neq 0 exit /b %errorlevel%
-
-                        echo "Everything is OK"
-                        '''
-                    }
-                }
-
-                stage('Windows-MSVCx64-cpp17') {
-
-                    agent {label 'windows'}
-
-                     steps {
-                        echo "Building on Windows in ${WORKSPACE}"
-                        checkout scm
-
-                        bat script: '''
-
-                        PATH=%PATH%;C:\\devel\\zlib-1.2.11\\Release;C:\\devel\\openssl\\lib-x64\\bin;C:\\local\\boost_1_64_0\\lib64-msvc-14.1
-                        rmdir /S /Q build
-                        mkdir build
-                        cd build
-                        cmake -DRESTC_CPP_USE_CPP17=ON -DCMAKE_PREFIX_PATH=c:/devel/zlib-1.2.11;c:/devel/zlib-1.2.11/Release;c:/devel/openssl;c:/devel/boost_1_64_0 -G "Visual Studio 15 Win64" ..
-                        if %errorlevel% neq 0 exit /b %errorlevel%
-                        cmake --build . --config Release
-                        if %errorlevel% neq 0 exit /b %errorlevel%
-                        ctest -C Release
-                        if %errorlevel% neq 0 exit /b %errorlevel%
-
-                        echo "Everything is OK"
-                        '''
-                    }
-                }
-
-                stage('Docker-build inside: Fedora') {
-                    agent {
-                        dockerfile {
-                            filename 'Dockerfile.fedora'
-                            dir 'ci/jenkins'
-                            label 'master'
+                        echo 'Getting ready to run tests'
+                        script {
+                            try {
+                                sh 'cd build && ctest --no-compress-output -T Test'
+                            } catch (exc) {
+                                echo 'Testing failed'
+                                currentBuild.result = 'UNSTABLE'
+                            }
                         }
                     }
+                }
 
-                    steps {
-                        echo "Building on Fedora in ${WORKSPACE}"
+                stage('Windows msvc14 C++14') {
+
+                    agent {label 'windows'}
+
+                     steps {
+                        echo "Building on Windows in ${WORKSPACE}"
                         checkout scm
-                        sh 'pwd; ls -la'
-                        sh 'rm -rf build'
-                        sh 'mkdir build'
-                        sh 'cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make -j2 && ctest --no-compress-output -T Test'
+
+                        bat script: '''
+                            PATH=%PATH%;C:\\devel\\zlib-1.2.11\\Release;C:\\devel\\openssl\\lib-x64\\bin;C:\\local\\boost_1_64_0\\lib64-msvc-14.1
+                            rmdir /S /Q build
+                            mkdir build
+                            cd build
+                            cmake -DCMAKE_PREFIX_PATH=c:/devel/zlib-1.2.11;c:/devel/zlib-1.2.11/Release;c:/devel/openssl;c:/devel/boost_1_64_0 -G "Visual Studio 15 Win64" ..
+                            if %errorlevel% neq 0 exit /b %errorlevel%
+                            cmake --build . --config Release
+                            if %errorlevel% neq 0 exit /b %errorlevel%
+
+                            echo "Build is OK"
+                        '''
+
+                        echo 'Getting ready to run tests'
+                        script {
+                            try {
+                                bat script: '''
+                                    PATH=%PATH%;C:\\devel\\zlib-1.2.11\\Release;C:\\devel\\openssl\\lib-x64\\bin;C:\\local\\boost_1_64_0\\lib64-msvc-14.1
+                                    cd build
+                                    ctest -C Release
+                                    if %errorlevel% neq 0 exit /b %errorlevel%
+                                '''
+                            } catch (exc) {
+                                echo 'Testing failed'
+                                currentBuild.result = 'UNSTABLE'
+                            }
+                        }
+                    }
+                }
+
+                stage('Windows msvc14 C++17') {
+
+                    agent {label 'windows'}
+
+                     steps {
+                        echo "Building on Windows in ${WORKSPACE}"
+                        checkout scm
+
+                        bat script: '''
+                            PATH=%PATH%;C:\\devel\\zlib-1.2.11\\Release;C:\\devel\\openssl\\lib-x64\\bin;C:\\local\\boost_1_64_0\\lib64-msvc-14.1
+                            rmdir /S /Q build
+                            mkdir build
+                            cd build
+                            cmake -DRESTC_CPP_USE_CPP17=ON -DCMAKE_PREFIX_PATH=c:/devel/zlib-1.2.11;c:/devel/zlib-1.2.11/Release;c:/devel/openssl;c:/devel/boost_1_64_0 -G "Visual Studio 15 Win64" ..
+                            if %errorlevel% neq 0 exit /b %errorlevel%
+                            cmake --build . --config Release
+                            if %errorlevel% neq 0 exit /b %errorlevel%
+                            echo "Build is OK"
+                        '''
+
+                        echo 'Getting ready to run tests'
+                        script {
+                            try {
+                                bat script: '''
+                                    PATH=%PATH%;C:\\devel\\zlib-1.2.11\\Release;C:\\devel\\openssl\\lib-x64\\bin;C:\\local\\boost_1_64_0\\lib64-msvc-14.1
+                                    cd build
+                                    ctest -C Release
+                                    if %errorlevel% neq 0 exit /b %errorlevel%
+                                '''
+                            } catch (exc) {
+                                echo 'Testing failed'
+                                currentBuild.result = 'UNSTABLE'
+                            }
+                        }
                     }
                 }
             }
-        }
 
-        stage('Clean-up') {
-            steps {
-                sh 'docker stop restc-cpp-squid restc-cpp-nginx restc-cpp-json'
+            post {
+                always {
+                    echo 'Shutting down test containers.'
+                    sh 'docker stop restc-cpp-squid restc-cpp-nginx restc-cpp-json'
+                }
             }
         }
     }
