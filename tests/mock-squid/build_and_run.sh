a0909e34dcc41d393be61259a02011144339e969
Jarle Aase <jarle@jgaa.com>
2018-02-16 08:00:43 +0200

    Fixed squid container for testing.

--- tests/mock-squid/build_and_run.sh
+++ tests/mock-squid/build_and_run.sh
@@ -5,17 +5,4 @@ docker rm restc-cpp-squid
 docker build . -t jgaa/restc-cpp-mock-squid
 docker run --name restc-cpp-squid --link restc-cpp-nginx:api.example.com -d -p 3003:3128  -t jgaa/restc-cpp-mock-squid
 
-# There is a problem during boot where squid is unable to
-# resolve the linked hostname
-sleep 2
-
-case "$OSTYPE" in
-  msys*)
-	echo "WINDOWS"
-	winpty docker exec -it restc-cpp-squid service squid3 restart
-	;;
-  *)
-	docker exec -it restc-cpp-squid service squid3 restart
-	;;
-esac
 
