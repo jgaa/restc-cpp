b0ff4d46d031311718e837978aa39dae9776db47
Jarle Aase <jarle@jgaa.com>
2018-06-13 09:33:34 +0300

    Pushing version to 0.9.2

--- ci/jenkins/Jenkinsfile.groovy
+++ ci/jenkins/Jenkinsfile.groovy
@@ -4,7 +4,7 @@ pipeline {
     agent { label 'master' }
 
     environment {
-        RESTC_CPP_VERSION = "0.9.1"
+        RESTC_CPP_VERSION = "0.9.2"
 
         // It is not possible to get the current IP number when running in the sandbox, and
         // Jenkinsfiles always runs in the sandbox.
