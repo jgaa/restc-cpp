6ed8f38da3b40c8e3903d0e6ddabcdf60b169026
Jarle Aase <jarle@jgaa.com>
2018-09-24 18:55:13 +0300

    Added test-file for RequestBuilder.

---
+++ tests/unit/RequestBuilder.cpp
@@ -0,0 +1,55 @@
+// Include before boost::log headers
+#include "restc-cpp/logging.h"
+
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/fusion/adapted.hpp>
+
+
+#include "restc-cpp/restc-cpp.h"
+#include "restc-cpp/RequestBuilder.h"
+
+#include "restc-cpp/test_helper.h"
+#include "lest/lest.hpp"
+
+using namespace std;
+using namespace restc_cpp;
+
+const lest::test specification[] = {
+
+STARTCASE(TestDataWithCString)
+{
+    RequestBuilder rb;
+    rb.Data("{}");
+    CHECK_EQUAL("{}", rb.GetData());
+
+} ENDCASE
+
+STARTCASE(TestDataWithCStringLen)
+{
+    RequestBuilder rb;
+    rb.Data("{}12345", 2);
+    CHECK_EQUAL("{}", rb.GetData());
+
+} ENDCASE
+
+STARTCASE(TestDataWithString)
+{
+
+    const string data{"{}"};
+
+    RequestBuilder rb;
+    rb.Data(data);
+    CHECK_EQUAL(data, rb.GetData());
+
+} ENDCASE
+
+
+}; // lest
+
+
+int main( int argc, char * argv[] )
+{
+    return lest::run( specification, argc, argv );
+}
