76bf7530c70fcbf57a2bad1c00bd39ff5a242518
Jarle Aase <jarle@jgaa.com>
2019-01-19 10:53:32 +0200

    Changed jenkins to use docker-compose for tests.

--- ci/jenkins/Jenkinsfile.groovy
+++ ci/jenkins/Jenkinsfile.groovy
@@ -15,7 +15,7 @@ pipeline {
     stages {
         stage('Prepare') {
             steps {
-                dh 'docker-compose -f ./ci/mock-backends/docker-compose.yml --build  up -d'
+                sh 'docker-compose -f ./ci/mock-backends/docker-compose.yml --build  up -d'
             }
         }
 
