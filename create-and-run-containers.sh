f51daf8f27238447468ea3b15ea1cb6508c7fb74
Jarle Aase <jarle@jgaa.com>
2020-03-22 16:00:32 +0200

    Fixed test setup to avoid using 'api' in /etc/hosts
    Closes #93

--- create-and-run-containers.sh
+++ create-and-run-containers.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 
+pushd ci/mock-backends
+
 docker ps > /dev/null
 if [ $? -eq 0 ]; then
     echo "Building and starting Docker containers for testing"
 else
     echo
-    echo "Cannot run docker commands. "
-    echo "Please install docker or give this user access to run docker commands!"
+    echo "Cannot run docker-compose commands. "
+    echo "Please install docker-compose or give this user access to run it"
+    popd
     exit -1
 fi
 
-BASE=`pwd`
-
-echo "JSON mock server"
-cd "$BASE/tests/mock-json"
-./build_and_run.sh
-
-echo "nginx mock server"
-cd "$BASE/tests/mock-nginx"
-./build_and_run.sh
-
-echo "squid mock server"
-cd "$BASE/tests/mock-squid/"
-./build_and_run.sh
-
+docker-compose build
+docker-compose up -d
+docker ps
+popd
---
+++ stop-containers.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pushd ci/mock-backends
+docker-compose down
+popd
