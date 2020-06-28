3202b7298691143c50d1635df501b0474366240c
Jarle Aase <jarle@jgaa.com>
2020-06-28 09:21:05 +0300

    Adding method to use existing yield

--- include/restc-cpp/restc-cpp.h
+++ include/restc-cpp/restc-cpp.h
@@ -387,7 +387,7 @@ public:
     template <typename T>
     T ProcessWithYield(const std::function<T (Context& ctx)>& fn, boost::asio::yield_context& yield) {
         auto ctx = Context::Create(yield, *this);
-        fn(yield);
+        return fn(yield);
     }
 
     virtual std::shared_ptr<ConnectionPool> GetConnectionPool() = 0;
