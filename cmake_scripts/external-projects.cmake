5a96f76a5126622e73b8307281dcd79bcc94af92
Jarle Aase <jarle@jgaa.com>
2023-01-23 14:00:28 +0200

    Removed 'lest' unit test framework from project

--- cmake_scripts/external-projects.cmake
+++ cmake_scripts/external-projects.cmake
@@ -26,18 +26,6 @@ ExternalProject_Add(
 
 set(EXTERNAL_RAPIDJSON_INCLUDE_DIR ${EXTERNAL_PROJECTS_PREFIX}/src/externalRapidJson/include/rapidjson)
 
-ExternalProject_Add(
-    externalLest
-    PREFIX "${EXTERNAL_PROJECTS_PREFIX}"
-    GIT_REPOSITORY "https://github.com/martinmoene/lest.git"
-    GIT_TAG "master"
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ""
-    LOG_DOWNLOAD ON
-    LOG_INSTALL ON
-    )
-
 ExternalProject_Add(externalLogfault
     PREFIX "${EXTERNAL_PROJECTS_PREFIX}"
     GIT_REPOSITORY "https://github.com/jgaa/logfault.git"
