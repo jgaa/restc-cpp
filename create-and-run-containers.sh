c7889aff49da888fce82ad52ccd17769c8398a37
Jarle Aase <jarle@jgaa.com>
2024-08-13 13:05:37 +0300

    Fixed test backend scripts to handle 'docker-compose' simetimes being 'docker compose'. We need to handle both cases.

--- create-and-run-containers.sh
+++ create-and-run-containers.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
-pushd ci/mock-backends
-
+# Check if Docker is running
 docker ps > /dev/null
-if [ $? -eq 0 ]; then
-    echo "Building and starting Docker containers for testing"
-else
+if [ $? -ne 0 ]; then
     echo
-    echo "Cannot run docker-compose commands. "
-    echo "Please install docker-compose or give this user access to run it"
+    echo "Cannot run docker commands. "
+    echo "Please install Docker or give this user access to run it"
     popd
     exit -1
 fi
 
-docker-compose stop
-docker-compose build
-docker-compose up -d
+# Determine the correct Docker Compose command
+DOCKER_COMPOSE="docker compose"
+$DOCKER_COMPOSE version > /dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+    DOCKER_COMPOSE="docker-compose"
+    $DOCKER_COMPOSE version > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo "Neither 'docker compose' nor 'docker-compose' is available. Please install Docker Compose."
+        popd
+        exit -1
+    fi
+fi
+
+echo "Using Docker Compose command: $DOCKER_COMPOSE"
+
+# Run Docker Compose commands
+pushd ci/mock-backends
+$DOCKER_COMPOSE stop
+$DOCKER_COMPOSE build
+$DOCKER_COMPOSE up -d
 docker ps
 popd
+
