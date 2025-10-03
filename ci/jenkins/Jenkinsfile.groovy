da8fd553f011d377da3da16b2087b24dce42457b
Jarle Aase <jarle@jgaa.com>
2025-10-03 15:22:00 +0300

    Working with test containers

--- ci/jenkins/Jenkinsfile.groovy
+++ ci/jenkins/Jenkinsfile.groovy
@@ -53,6 +53,74 @@ pipeline {
                     }
                 }
 
+                stage('Ubuntu Plucky') {
+                    agent {
+                        dockerfile {
+                            filename 'Dockerfile.ubuntu-plucky'
+                            dir 'ci/jenkins'
+                            label 'docker'
+                            args '-u root'
+                        }
+                    }
+
+                    options {
+                        timeout(time: 30, unit: "MINUTES")
+                    }
+
+                    steps {
+                        echo "Building on ubuntu-plucky-AMD64 in ${NODE_NAME} --> ${WORKSPACE}"
+                        checkout scm
+                        sh 'pwd; ls -la'
+                        sh 'rm -rf build'
+                        sh 'mkdir build'
+                        sh 'cd build && cmake -DCMAKE_BUILD_TYPE=Release  .. && make -j $(nproc)'
+
+                        echo 'Getting ready to run tests'
+                        script {
+                            try {
+                                sh 'cd build && ctest --no-compress-output -T Test'
+                            } catch (exc) {
+
+                                unstable(message: "${STAGE_NAME} - Testing failed")
+                            }
+                        }
+                    }
+                }
+
+                stage('Ubuntu Plucky MT CTX') {
+                    agent {
+                        dockerfile {
+                            filename 'Dockerfile.ubuntu-plucky'
+                            dir 'ci/jenkins'
+                            label 'docker'
+                            args '-u root'
+                        }
+                    }
+
+                    options {
+                        timeout(time: 30, unit: "MINUTES")
+                    }
+
+                    steps {
+                        echo "Building on ubuntu-plucky-MT-AMD64 in ${NODE_NAME} --> ${WORKSPACE}"
+                        checkout scm
+                        sh 'pwd; ls -la'
+                        sh 'rm -rf build'
+                        sh 'mkdir build'
+                        sh 'cd build && cmake -DRESTC_CPP_THREADED_CTX=ON -DCMAKE_BUILD_TYPE=Release  .. && make -j $(nproc)'
+
+                        echo 'Getting ready to run tests'
+                        script {
+                            try {
+                                sh 'cd build && ctest --no-compress-output -T Test'
+                            } catch (exc) {
+
+                                unstable(message: "${STAGE_NAME} - Testing failed")
+                            }
+                        }
+                    }
+                }
+
                 stage('Ubuntu Noble') {
                     agent {
                         dockerfile {
@@ -121,7 +189,7 @@ pipeline {
                     }
                 }
 
-                 stage('Ubuntu Jammy') {
+                stage('Ubuntu Jammy') {
                     agent {
                         dockerfile {
                             filename 'Dockefile.ubuntu-jammy'
@@ -189,10 +257,10 @@ pipeline {
                     }
                 }
 
-                stage('Ubuntu Bionic') {
+                stage('Ubuntu Jammy') {
                     agent {
                         dockerfile {
-                            filename 'Dockerfile.ubuntu-bionic'
+                            filename 'Dockefile.ubuntu-jammy'
                             dir 'ci/jenkins'
                             label 'docker'
                             args '-u root'
@@ -204,11 +272,12 @@ pipeline {
                     }
 
                     steps {
-                        echo "Building on ubuntu-bionic-AMD64 in ${NODE_NAME} --> ${WORKSPACE}"
+                        echo "Building on ubuntu-jammy-AMD64 in ${NODE_NAME} --> ${WORKSPACE}"
                         checkout scm
+                        sh 'pwd; ls -la'
                         sh 'rm -rf build'
                         sh 'mkdir build'
-                        sh 'cd build && cmake -DGTEST_TAG=release-1.12.0 -DCMAKE_BUILD_TYPE=Release .. && make -j $(nproc)'
+                        sh 'cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make -j $(nproc)'
 
                         echo 'Getting ready to run tests'
                         script {
@@ -575,51 +644,55 @@ pipeline {
                     }
                 }
 
-                // stage('Windows X64 with vcpkg MT CTX') {
 
-                //     agent {label 'windows'}
+                stage('Windows X64 with vcpkg MT CTX') {
 
-                //     options {
-                //         // vcpkg now installs and compiles pretty much everything that exists on github if you ask it to prepare boost and openssl.
-                //         // It's becoming as bad as js and npm.
-                //         timeout(time: 60, unit: "MINUTES")
-                //     }
+                    agent {label 'windows'}
 
-                //      steps {
-                //         echo "Building on Windows in ${WORKSPACE}"
-                //         checkout scm
+                    options {
+                        // vcpkg now installs and compiles pretty much everything that exists on github if you ask it to prepare boost and openssl.
+                        // It's becoming as bad as js and npm.
+                        timeout(time: 60, unit: "MINUTES")
+                    }
 
-                //         bat script: '''
-                //             PATH=%PATH%;C:\\Program Files\\CMake\\bin;C:\\src\\vcpkg;C:\\Program Files\\Git\\bin
-                //             vcpkg integrate install
-                //             vcpkg install rapidjson gtest zlib openssl boost --triplet x64-windows
-                //             if %errorlevel% neq 0 exit /b %errorlevel%
-                //             rmdir /S /Q build
-                //             mkdir build
-                //             cd build
-                //             cmake -DRESTC_CPP_THREADED_CTX=ON -DCMAKE_TOOLCHAIN_FILE=C:/src/vcpkg/scripts/buildsystems/vcpkg.cmake ..
-                //             if %errorlevel% neq 0 exit /b %errorlevel%
-                //             cmake --build . --config Release
-                //             if %errorlevel% neq 0 exit /b %errorlevel%
-                //             echo "Build is OK"
-                //         '''
+                     steps {
+                        echo "Building on Windows in ${WORKSPACE}"
+                        checkout scm
 
-                //         echo 'Getting ready to run tests'
-                //         script {
-                //             try {
-                //                 bat script: '''
-                //                     PATH=%PATH%;C:\\src\\vcpkg\\installed\\x64-windows\\bin;C:\\Program Files\\CMake\\bin
-                //                     cd build
-                //                     ctest -C Release
-                //                     if %errorlevel% neq 0 exit /b %errorlevel%
-                //                 '''
-                //             } catch (exc) {
+                        bat script: '''
+                            PATH=C:\\src\\vcpkg;%PATH%;C:\\Program Files\\CMake\\bin;C:\\Program Files\\Git\\bin
+                            git -C C:\\src\\vcpkg pull --rebase
+                            vcpkg upgrade --no-dry-run
+                            vcpkg remove --outdated
+                            vcpkg integrate install
+                            vcpkg install rapidjson gtest zlib openssl boost-program-options boost-filesystem boost-date-time boost-coroutine boost-context boost-chrono boost-asio boost-system boost-log --triplet x64-windows
+                            if %errorlevel% neq 0 exit /b %errorlevel%
+                            rmdir /S /Q build
+                            mkdir build
+                            cd build
+                            cmake -DRESTC_CPP_THREADED_CTX=ON -DCMAKE_TOOLCHAIN_FILE=C:/src/vcpkg/scripts/buildsystems/vcpkg.cmake ..
+                            if %errorlevel% neq 0 exit /b %errorlevel%
+                            cmake --build . --config Release
+                            if %errorlevel% neq 0 exit /b %errorlevel%
+                            echo "Build is OK"
+                        '''
 
-                //                 unstable(message: "${STAGE_NAME} - Testing failed")
-                //             }
-                //         }
-                //     }
-                // }
+                        echo 'Getting ready to run tests'
+                        script {
+                            try {
+                                bat script: '''
+                                    PATH=%PATH%;C:\\src\\vcpkg\\installed\\x64-windows\\bin;C:\\Program Files\\CMake\\bin
+                                    cd build
+                                    ctest -C Release
+                                    if %errorlevel% neq 0 exit /b %errorlevel%
+                                '''
+                            } catch (exc) {
+
+                                unstable(message: "${STAGE_NAME} - Testing failed")
+                            }
+                        }
+                    }
+                }
 
             } // parallel
 
